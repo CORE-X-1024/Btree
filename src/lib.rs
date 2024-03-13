f97f50ed36e379a588ba23a6e40b144b3ba95834
Narongchai Cherdchoo <nache7383@uit.no>
2024-03-14 00:37:48 +0100

    ny bug

--- src/lib.rs
+++ src/lib.rs
@@ -48,12 +48,12 @@ impl Node{
                         //sjekk antall keys i barna
                         match self.edges[i].keys.len() {
                             3 => {
+                                //todo sjekk edge igjen
                                 //flytt median opp
                                 self.keys.insert(i, self.edges[i].keys.remove(1).unwrap());
                                 // og Splitt node
                                 let key_0 = self.edges[i].keys.pop_front().unwrap();
                                 self.edges.insert(i, Node::new());
-
                                 self.edges[i].keys.push_front(key_0);
                                 //insert key
                                 self.insert_key(key);
@@ -98,10 +98,9 @@ impl BTree{
 
     pub fn add(&mut self, key : i32){
 
-        if self.root.keys.len() < 3 {
-            self.root.insert_key(key);
+        self.root.insert_key(key);
 
-        } else {
+        if self.root.keys.len() == 3{
             match self.root.edges.len() {
                 0 => {
                     let mut child0 = Node::new();
@@ -154,18 +153,22 @@ mod tests {
     fn it_works() {
 
         let mut tree = BTree::new();
-        for i in 1..= 10 {
+        for i in 1..= 11 {
             tree.add(i);
         }
 
-        println!("{:?}", tree.root.keys);
+        println!("root : {:?}", tree.root.keys);
         let len = tree.root.edges.len();
         for i in 0..len  {
             println!("leaves{}: {:?}", i, tree.root.edges[i].keys);
             let len2 = tree.root.edges[i].edges.len();
             for j in 0..len2  {
-
                 println!("leaves{}.leefs{}: {:?}", i, j, tree.root.edges[i].edges[j].keys);
+
+                let len3 = tree.root.edges[i].edges[j].edges.len();
+                for k in 0..len3  {
+                    println!("leaves{}.leefs{}.more{}: {:?}", i, j, k, tree.root.edges[i].edges[j].edges[k].keys);
+                }
             }
 
         }
