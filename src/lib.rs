d936041a0e8b8fd947b01462bcca168d9162c39c
Narongchai Cherdchoo <nache7383@uit.no>
2024-03-13 23:57:10 +0100

    BUGS FIXED

--- src/lib.rs
+++ src/lib.rs
@@ -25,110 +25,67 @@ impl Node{
             0 => {
                 //self sort
                 let len_key = self.keys.len();
-                if self.keys.is_empty() {
+                if len_key == 0{
                     self.keys.push_back(key);
                 } else {
                     for i in 0..len_key {
                         if key < self.keys[i] {
                             self.keys.insert(i, key);
-                            break;
+                            return;
                         }
                     }
-
                     if key > self.keys[len_key - 1] {
                         self.keys.push_back(key);
                     }
                 }
             },
-
-            //To barn
-            2 => {
+            _ => {
                 //sjekk enhverbarn sin keys.
-                if key < self.keys[0] {
-
-                    //sjekk antall keys i barna
-                    match self.edges[0].keys.len() {
-                        3 => {
-                            //flytt median opp
-                            self.keys.insert(0, self.edges[0].keys.remove(1).unwrap());
-                            // og Splitt node
-                            let key_0 = self.edges[0].keys.pop_front().unwrap();
-                            self.edges.insert(0, Node::new());
-                            self.edges[0].keys.push_front(key_0);
-                            //insert key
-                            self.edges[0].insert_key(key);
-                        },
-                        _ => {
-                            self.edges[0].insert_key(key);
-                        },
-                    }
-                } else {
-
-                    //sjekk antall keys i barna
-                    match self.edges[1].keys.len() {
-                        3 => {
-                            //flytt median opp
-                            self.keys.push_back(self.edges[1].keys.remove(1).unwrap());
-                            // og Splitt node
-                            let key_1 = self.edges[1].keys.pop_back().unwrap();
-                            self.edges.push_back(Node::new());
-                            self.edges[2].keys.push_front(key_1);
-                            //insert key
-                            self.edges[2].insert_key(key);
-                        },
-                        _ => {
-                            self.edges[1].insert_key(key);
-                        },
-                    }
-                }
-            },
-            //Tre barn
-            3 => {
-                //Finn hvilket egde vi skal gå
-                let key_len = self.keys.len();
-                for i in 0..key_len {
+                let len_key = self.keys.len();
+                for i in 0..len_key {
 
                     if key < self.keys[i] {
-                        //Hvis barnen har max keys //Max keys --> splits and move up
-                        if self.edges[i].keys.len() == 3 {
-                            //Flytt median opp til keys, front.
-                            self.keys.insert(i, self.edges[i].keys.remove(1).unwrap());
-                            //refere til en ny egde
-                            let key_0 = self.edges[i].keys.pop_front().unwrap();
-                            self.edges.insert(i, Node::new());
-                            self.edges[i].insert_key(key_0);
-                            //innsett ny key etter splitt
-                            self.edges[i].insert_key(key);
-                            return;
+                        //sjekk antall keys i barna
+                        match self.edges[i].keys.len() {
+                            3 => {
+                                //flytt median opp
+                                self.keys.insert(i, self.edges[i].keys.remove(1).unwrap());
+                                // og Splitt node
+                                let key_0 = self.edges[i].keys.pop_front().unwrap();
+                                self.edges.insert(i, Node::new());
 
-                        } else {
+                                self.edges[i].keys.push_front(key_0);
+                                //insert key
+                                self.insert_key(key);
+                                return;
+                            },
+                            _ => {
+                                self.insert_key(key);
+                                return;
+                            },
+                        }
+                    }
 
-                            self.edges[i].insert_key(key);
-                            return;
+                    if key > self.keys[len_key - 1]{
+
+                        match self.edges[len_key].keys.len() {
+                            3 => {
+                                //flytt median opp
+                                self.keys.push_back(self.edges[len_key].keys.remove(1).unwrap());
+                                // og Splitt node
+                                let key_1 = self.edges[len_key].keys.pop_back().unwrap();
+                                self.edges.insert(len_key + 1, Node::new());
+                                self.edges[len_key + 1].insert_key(key_1);
+                                //insert key
+                                self.insert_key(key);
+                            },
+                            _ => {
+                                self.edges[len_key].insert_key(key);
+                            },
                         }
                     }
                 }
-
-                //Hvis keys er høyest
-                if key > self.keys[1] {
-                    //Sjekk om barn har maks keys
-                    if self.edges[key_len].keys.len() == 3 {
-
-                        self.keys.push_back(self.edges[key_len].keys.remove(1).unwrap());
-                        let key_2 = self.edges[key_len].keys.pop_back().unwrap();
-
-                        self.edges.push_back( Node::new());
-                        self.edges[key_len+1].insert_key(key_2);
-
-                        self.edges[key_len+1].insert_key(key);
-                    } else {
-                        self.edges[key_len].insert_key(key);
-                    }
-                }
             }
-
-            //todo 4 barn
-            _ => panic!("{}", self.edges.len()),
         }
     }
 
@@ -141,13 +98,10 @@ impl BTree{
 
     pub fn add(&mut self, key : i32){
 
-        if self.root.keys.len()< 3 {
-
+        if self.root.keys.len() < 3 {
             self.root.insert_key(key);
 
-
         } else {
-
             match self.root.edges.len() {
                 0 => {
                     let mut child0 = Node::new();
@@ -181,10 +135,9 @@ impl BTree{
                     self.root.edges.push_back(child1);
 
                     self.root.insert_key(key);
-
                 }
                 //todo 2,3  barn
-                _ => panic!("{}",self.root.edges.len()),
+                _ => panic!("{}", self.root.edges.len()),
             }
         }
     }
@@ -201,14 +154,19 @@ mod tests {
     fn it_works() {
 
         let mut tree = BTree::new();
-        for i in 1..= 13 {
+        for i in 1..= 10 {
             tree.add(i);
         }
 
         println!("{:?}", tree.root.keys);
         let len = tree.root.edges.len();
         for i in 0..len  {
-            println!("Egde {}: {:?}", i, tree.root.edges[i].keys);
+            println!("leaves{}: {:?}", i, tree.root.edges[i].keys);
+            let len2 = tree.root.edges[i].edges.len();
+            for j in 0..len2  {
+
+                println!("leaves{}.leefs{}: {:?}", i, j, tree.root.edges[i].edges[j].keys);
+            }
 
         }
     }
